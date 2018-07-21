db8f41a17dc8b2ac79e7f31adef386aa0cb4fe4c
Aubron Wood <aubron@daemon.group>
2018-07-20 22:53:24 -0400

    Update ScoreShots section with product timeline.

--- frontend/src/presentational/ScoreShotsTimeline.js
+++ frontend/src/presentational/ScoreShotsTimeline.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import SlideShow from '../containers/SlideShow';
 import history from '../dict/scoreshotsHistory';
 
 const styles = theme => ({
@@ -14,8 +16,9 @@ const styles = theme => ({
 const ScoreShotsTimeline = ({classes}) => (
   <div style={{
     borderLeft: '1px dashed rgba(0, 0, 0, 0.87)',
-    width: '80%',
+    width: '90%',
     margin: 'auto',
+    paddingTop: 1,
   }}>
     {history.map((event) => (
       <Card className={classes.card} key={event.date}>
@@ -23,6 +26,23 @@ const ScoreShotsTimeline = ({classes}) => (
           title={event.title}
           subheader={event.date}
         />
+        {
+          event.photos ?
+            <SlideShow photos={event.photos} /> :
+          null
+        }
+        {
+          event.video ?
+            <div style={{textAlign: 'center'}}>
+              <video style={{maxWidth: '100%'}} src={event.video} autoPlay loop muted />
+            </div> :
+          null
+        }
+        <CardContent>
+          {event.components.map((component) => (
+            component
+          ))}
+        </CardContent>
       </Card>
     ))}
   </div>
