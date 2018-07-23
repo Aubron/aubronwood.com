08deb350006f4a81fdfa67d79912a93be40dcc22
Aubron Wood <aubron@daemon.group>
2018-07-22 20:07:20 -0400

    Remove serviceworker import, fix build.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './views/Main';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Main />, document.getElementById('root'));
